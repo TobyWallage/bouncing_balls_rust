24724b67fb19953c125189ebb16359a7849424c7
Toby Wallage <tlw31@sussex.ac.uk>
2023-07-16 14:31:03 +0100

    Separated Ball Spawning to separate function

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+use bevy::input::touch::*;
 use bevy::prelude::*;
 use bevy::sprite::MaterialMesh2dBundle;
 use bevy::time::Stopwatch;
 use bevy::utils::HashMap;
 use bevy::window::PrimaryWindow;
-use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use rand::Rng;
 
 pub const SPEED: f32 = 400.0;
 pub const GRAVITY: Vec3 = Vec3::new(0.0, -600.0, 0.0);
 pub const DAMPENING: f32 = 0.998;
-pub const FIXED_TIME: f32 = 1.0/600.0;
+pub const FIXED_TIME: f32 = 1.0 / 600.0;
 
 fn main() {
     App::new()
@@ -21,12 +22,8 @@ fn main() {
         .add_startup_system(spawn_rate_limiter)
         // .add_startup_system(spawn_ball)
         .add_systems(
-            (
-                ball_movement,
-                ball_check_border,
-                check_ball_collision,
-            )
-            .in_schedule(CoreSchedule::FixedUpdate)
+            (ball_movement, ball_check_border, check_ball_collision)
+                .in_schedule(CoreSchedule::FixedUpdate),
         )
         .insert_resource(FixedTime::new_from_secs(FIXED_TIME))
         .add_system(spawn_ball)
@@ -42,17 +39,20 @@ pub struct Ball {
 }
 
 #[derive(Component)]
-pub struct SpawnRateLimiter{
-    pub time:Stopwatch
+pub struct SpawnRateLimiter {
+    pub time: Stopwatch,
 }
 
-pub fn spawn_rate_limiter(mut commands: Commands){
-    commands.spawn(SpawnRateLimiter{time: Stopwatch::new()});
+pub fn spawn_rate_limiter(mut commands: Commands) {
+    commands.spawn(SpawnRateLimiter {
+        time: Stopwatch::new(),
+    });
 }
 
 pub fn spawn_ball(
     mut commands: Commands,
     mouse_input: Res<Input<MouseButton>>,
+    touch_input: Res<Touches>,
     window_query: Query<&Window, With<PrimaryWindow>>,
     time: Res<Time>,
     mut spawn_rate_q: Query<&mut SpawnRateLimiter>,
@@ -61,40 +61,64 @@ pub fn spawn_ball(
 ) {
     let window = window_query.get_single().unwrap();
     let mut spawn_rate_limiter = spawn_rate_q.get_single_mut().unwrap();
-    if spawn_rate_limiter.time.tick(time.delta()).elapsed_secs() < 0.2{
+    if spawn_rate_limiter.time.tick(time.delta()).elapsed_secs() < 0.2 {
         return;
     }
 
+    // mouse input logic
     if let Some(mouse_position) = window.cursor_position() {
-        if mouse_input.just_released(MouseButton::Left)  || mouse_input.pressed(MouseButton::Right){
+        if mouse_input.just_released(MouseButton::Left) || mouse_input.pressed(MouseButton::Right) {
             println!(
                 "Cursor clicked inside the primary window, at {:?}",
                 mouse_position
             );
-
-            spawn_rate_limiter.time.reset();
-
-            let rand_radius: f32 = rand::thread_rng().gen_range(5.0..=20.0);
-            let rand_velcoity: Vec3 = Vec3::new(
-                rand::thread_rng().gen_range(-40.0..=40.0),
-                rand::thread_rng().gen_range(-40.0..=40.0),
-                0.0,
-            );
-
-            commands.spawn((
-                MaterialMesh2dBundle {
-                    mesh: meshes.add(shape::Circle::new(rand_radius).into()).into(),
-                    material: materials.add(ColorMaterial::from(Color::ORANGE_RED)),
-                    transform: Transform::from_xyz(mouse_position.x, mouse_position.y, 0.0),
-                    ..default()
-                },
-                Ball {
-                    radius: rand_radius,
-                    velocity: rand_velcoity,
-                },
-            ));
+            spawn_ball_function(
+                &mouse_position,
+                &mut commands,
+                &mut spawn_rate_limiter,
+                &mut meshes,
+                &mut materials,
+            )
         }
     }
+    // touch input logic
+
+    for touch in touch_input.iter(){
+        info!("Touch {:?}", touch);
+        spawn_ball_function(&touch.position(), &mut commands, &mut spawn_rate_limiter, &mut meshes, &mut materials)
+    }
+
+}
+
+fn spawn_ball_function(
+    // This function is to seperate the actually ball spawning from the system that actually decided where/when to spawn a ball
+    position: &Vec2,
+    commands: &mut Commands,
+    spawn_rate_limiter: &mut Mut<'_, SpawnRateLimiter>,
+    meshes: &mut ResMut<Assets<Mesh>>,
+    materials: &mut ResMut<Assets<ColorMaterial>>,
+) {
+    spawn_rate_limiter.time.reset();
+
+    let rand_radius: f32 = rand::thread_rng().gen_range(5.0..=20.0);
+    let rand_velcoity: Vec3 = Vec3::new(
+        rand::thread_rng().gen_range(-40.0..=40.0),
+        rand::thread_rng().gen_range(-40.0..=40.0),
+        0.0,
+    );
+
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::new(rand_radius).into()).into(),
+            material: materials.add(ColorMaterial::from(Color::ORANGE_RED)),
+            transform: Transform::from_xyz(position.x, position.y, 0.0),
+            ..default()
+        },
+        Ball {
+            radius: rand_radius,
+            velocity: rand_velcoity,
+        },
+    ));
 }
 
 pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
@@ -108,7 +132,8 @@ pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Pr
 pub fn ball_movement(mut ball_query: Query<(&mut Transform, &mut Ball)>) {
     for (mut transform, mut ball) in ball_query.iter_mut() {
         let current_velocity = Vec3::new(ball.velocity.x, ball.velocity.y, 0.0);
-        transform.translation += current_velocity * FIXED_TIME + (0.5 * GRAVITY*FIXED_TIME*FIXED_TIME);
+        transform.translation +=
+            current_velocity * FIXED_TIME + (0.5 * GRAVITY * FIXED_TIME * FIXED_TIME);
         ball.velocity += GRAVITY * FIXED_TIME;
     }
 }
@@ -152,61 +177,62 @@ pub fn print_fps(time: Res<Time>, key_input: Res<Input<KeyCode>>) {
 }
 
 pub fn check_ball_collision(mut ball_query: Query<(Entity, &mut Transform, &mut Ball)>) {
-
-    let mut translation_map : HashMap<Entity, Vec3> = HashMap::new();
-    let mut velocity_map :HashMap<Entity, Vec3> = HashMap::new();
+    let mut translation_map: HashMap<Entity, Vec3> = HashMap::new();
+    let mut velocity_map: HashMap<Entity, Vec3> = HashMap::new();
 
     for (entity_1, transform_1, ball_1) in ball_query.iter() {
-
         let mut cummulative_translation = Vec3::new(0.0, 0.0, 0.0);
         let mut cummulative_velocity = Vec3::new(0.0, 0.0, 0.0);
 
-        for (entity_2, transform_2, ball_2) in ball_query.iter(){
-            if entity_1 == entity_2{
+        for (entity_2, transform_2, ball_2) in ball_query.iter() {
+            if entity_1 == entity_2 {
                 continue;
             };
 
             let distance = transform_1.translation.distance(transform_2.translation);
 
-            if distance > (ball_1.radius + ball_2.radius){
+            if distance > (ball_1.radius + ball_2.radius) {
                 continue;
             }
 
             let velocity_diff = ball_1.velocity - ball_2.velocity;
             let position_diff = transform_1.translation - transform_2.translation;
 
-            if position_diff.length() == 0.0{
+            if position_diff.length() == 0.0 {
                 continue;
             }
 
             // we sum up the total translation and velocity change from all collisions
-            cummulative_translation -= 0.4*(position_diff.length() - (ball_1.radius + ball_2.radius))*position_diff.normalize();
+            cummulative_translation -= 0.4
+                * (position_diff.length() - (ball_1.radius + ball_2.radius))
+                * position_diff.normalize();
             // transform_2.translation += 0.5*(position_diff.length() - (ball_1.radius + ball_2.radius))*position_diff.normalize();
-            cummulative_velocity -= ((velocity_diff).dot(position_diff) / (position_diff.length_squared())) * position_diff;
+            cummulative_velocity -= ((velocity_diff).dot(position_diff)
+                / (position_diff.length_squared()))
+                * position_diff;
             // ball_2.velocity -= ((-velocity_diff).dot(-position_diff) / (-position_diff.length_squared())) * -position_diff;
-
         }
         // insert cumulated collisions into hashmap to apply mutably later
         translation_map.insert(entity_1, cummulative_translation);
         velocity_map.insert(entity_1, cummulative_velocity);
-        
     }
 
     for (entity, mut transform, mut ball) in ball_query.iter_mut() {
-        ball.velocity += DAMPENING*(*velocity_map.get(&entity).unwrap());
+        ball.velocity += DAMPENING * (*velocity_map.get(&entity).unwrap());
         transform.translation += *translation_map.get(&entity).unwrap();
     }
-
 }
 
-pub fn change_ball_color( ball_material_query: Query<(&Ball, &Handle<ColorMaterial>)>, mut materials: ResMut<Assets<ColorMaterial>>){
-    for (ball, color_handle) in ball_material_query.iter(){
+pub fn change_ball_color(
+    ball_material_query: Query<(&Ball, &Handle<ColorMaterial>)>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    for (ball, color_handle) in ball_material_query.iter() {
         let color = &mut materials.get_mut(color_handle).unwrap().color;
-        let new_color = Color::hsla((ball.velocity.length()/9.0) % 360.0, 0.9,0.6,1.0);
+        let new_color = Color::hsla((ball.velocity.length() / 9.0) % 360.0, 0.9, 0.6, 1.0);
         color.set_r(new_color.r());
         color.set_g(new_color.g());
         color.set_b(new_color.b());
         color.set_a(new_color.a());
-
     }
 }
